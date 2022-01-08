c7f0fe66486b66cc3eb275f611eeb685bb66e06b
Matheson Steplock <ikifar2012@users.noreply.github.com>
2022-01-08 01:01:04 +0000

    basic config and cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -79,12 +78,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: 'What are we fixing today, Discord',
+                href: 'https://discord.gg/Uhmhu3B',
               },
               {
                 label: 'Twitter',
@@ -96,17 +91,13 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Matheson Steplock. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
