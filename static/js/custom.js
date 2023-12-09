970f63dc35013996b2e721efbb2eed4437d8bb1a
Matheson Steplock <ikifar2012@users.noreply.github.com>
2023-12-09 04:42:22 +0000

    Add console logs to track scrolling behavior

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
   var navbar = document.querySelector(".navbar.navbar--fixed-top");
 
   window.addEventListener("scroll", function () {
+    console.log("scrolling");
+    // Set the current scroll position
+    console.log(window.scrollY);
     var scroll = window.scrollY;
     // Adjust the scroll position and blur value as needed
     if (scroll > 0) {
