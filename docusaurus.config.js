280914b0cc517cc166a1bb0486527b3d9bb245a2
Matheson Steplock <ikifar2012@users.noreply.github.com>
2023-12-09 06:02:14 +0000

    Update clientModules path in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/logo.png',
   organizationName: 'ikifar2012', // Usually your GitHub org/user name.
   projectName: 'addons.mathesonsteplock.ca', // Usually your repo name.
-  clientModules: ['./src/js/custom.js'],
+  clientModules: ['js/custom.js'],
   presets: [
     [
       'classic',
